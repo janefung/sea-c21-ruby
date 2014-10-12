0e69a2e2b7f19ad1013f710379313c978d647a8c
Jane Fung <janefungpublic@gmail.com>
2014-10-11 22:09:53 -0700

    working class2 assignments

--- lib/class2/exercise2.rb
+++ lib/class2/exercise2.rb
@@ -3,3 +3,5 @@
 # Assuming leap-years don't exist, display the minutes in a decade.
 #
 # TIP: I expect you to use a mathematical equation.
+
+puts 24 * 60 * 365 * 10
--- lib/class2/exercise5.rb
+++ lib/class2/exercise5.rb
@@ -9,3 +9,9 @@
 #   What's your favorite number?
 #   42
 #   That's ok, I guess, but isn't 43 just a bit better?
+
+puts "What's your favorite number?"
+favorite_number = gets.chomp
+fav_num_plus_1 = favorite_number.to_i + 1
+
+puts "That's ok, I guess, but isn't #{fav_num_plus_1} just a bit better?"
