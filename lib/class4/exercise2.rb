0772c8f81b8716ccf8c6c61d6cb82dba3edfff30
Jane Fung <janefungpublic@gmail.com>
2014-10-19 19:38:58 -0700

    class4 assignment changes

--- lib/class4/exercise2.rb
+++ lib/class4/exercise2.rb
@@ -17,3 +17,16 @@
 # TIP #2: Use the `string.ljust` method from class 2 exercise 7.
 #
 # TIP #3: The width of each line is 50 characters.
+
+hits = [
+  ['Can I Kick It?', 'A Tribe Called Quest', '1991'],
+  ['Rump Shaker',    'Wreckx-n-Effect',      '1992'],
+  ['Check Yo Self',  'Ice Cube',             '1993'],
+  ['Regulate',       'Warren G & Nate Dogg', '1994'],
+  ['I Got 5 On It',  'Luniz',                '1995'],
+  ['Ready Or Not',   'The Fugees',           '1996']
+]
+
+hits.each do |hit|
+  puts hit[0].ljust(20) + hit[1].ljust(20) + hit[2].rjust(10)
+end
