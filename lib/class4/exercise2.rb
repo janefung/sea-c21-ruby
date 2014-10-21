893f517af5cb0a674ac935d77883d0c20dc196dd
Jane Fung <janefungpublic@gmail.com>
2014-10-20 21:17:31 -0700

    class5 assignment1

--- lib/class4/exercise2.rb
+++ lib/class4/exercise2.rb
@@ -5,12 +5,12 @@
 # Write a program that displays the following table of 90's Hip-Hop hits:
 #
 #   $ ruby exercise2.rb
-#   Can I Kick It?      A Tribe Called Quest      1991
-#   Rump Shaker         Wreckx-n-Effect           1992
-#   Check Yo Self       Ice Cube                  1993
-#   Regulate            Warren G & Nate Dogg      1994
-#   I Got 5 On It       Luniz                     1995
-#   Ready Or Not        The Fugees                1996
+#   Can I Kick It?      A Tribe Called Quest     1991
+#   Rump Shaker         Wreckx-n-Effect          1992
+#   Check Yo Self       Ice Cube                 1993
+#   Regulate            Warren G & Nate Dogg     1994
+#   I Got 5 On It       Luniz                    1995
+#   Ready Or Not        The Fugees               1996
 #
 # TIP #1: Hold all of the information -- song, artist, and year -- in an array.
 #
@@ -34,7 +34,7 @@ end
 # Ryan's solution...
 
 # hits.each do |hit|
-#   song, artist, year = hit #destructuring asignment
+#   song, artist, year = hit  #destructuring asignment
 #
 #   puts song.ljust(20) + artist.ljust(26) + year.ljust(4)
 # end
