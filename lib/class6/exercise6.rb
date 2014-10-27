b09ed935e47adce1f865e49975f56f0212451737
Jane Fung <janefungpublic@gmail.com>
2014-10-26 19:04:00 -0700

    class6 assignment6 changes

--- lib/class6/exercise6.rb
+++ lib/class6/exercise6.rb
@@ -30,7 +30,8 @@ def load
 end
 
 def update(key, value)
-  load.merge(key.to_sym => value)
+  person = load.merge(key.to_sym => value)
+  File.write(database, person.to_yaml)
 end
 
 input1, input2 = ARGV
--- lib/class6/exercise7.rb
+++ lib/class6/exercise7.rb
@@ -23,11 +23,11 @@
 require 'yaml'
 
 def database
-  '/replace/me'
+  File.absolute_path('../database.yml', __FILE__)
 end
 
 def load
-  { replace: 'me' }
+  YAML.load_file(database)
 end
 
 def remove(key)
